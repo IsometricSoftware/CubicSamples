051a474df71e47c1efff22c253b90fa32cb05598
Kofu145g <kofu145g@gmail.com>
2022-07-01 03:05:49 -0700

    Added pong sample project

---
+++ Pong/Pong/Program.cs
@@ -0,0 +1,18 @@
+﻿using Cubic;
+using System.Drawing;
+using Cubic.Scenes;
+using Pong.Scenes;
+using Cubic.Windowing;
+
+// See https://aka.ms/new-console-template for more information
+
+GameSettings settings = new GameSettings()
+{
+    Title = "Pong demo",
+    Size = new Size(600, 400),
+    AudioChannels = 4
+};
+
+using CubicGame game = new CubicGame(settings);
+SceneManager.RegisterScene<MainScene>("main");
+game.Run();
